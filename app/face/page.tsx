fc8fbbdcf3cf9c27163695fb5dd54d84aee1b020
Bodo Kaiser <i@bodokaiser.io>
2025-12-22 14:33:24 +0100

    added images

--- app/face/page.tsx
+++ app/face/page.tsx
@@ -8,7 +8,7 @@ export default function ContactPage() {
       <br />
       <br />
       <Image
-        src="/DSC_0907.png"
+        src="/DSC_0907.jpg"
         alt="Madero"
         width={700}
         height={700}
@@ -70,7 +70,7 @@ export default function ContactPage() {
         </div>
       </div>
       <Image
-        src="/DSC_0902.png"
+        src="/DSC_0902.jpg"
         alt="Madero"
         width={700}
         height={700}
@@ -141,7 +141,7 @@ export default function ContactPage() {
       <br />
       <br />
       <Image
-        src="/DSC_0892.png"
+        src="/DSC_0892.jpg"
         alt="Madero"
         width={700}
         height={700}
